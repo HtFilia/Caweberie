7f8c96b82a53e4217b13ddcaf27a08fb17e684fe
Lucas Lebihan <htfilia@gmail.com>
2020-01-22 00:23:39 +0100

    C'est encore plus beau

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,26 +16,37 @@ public class Application extends Controller {
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
+    public static boolean isConnected() {
+        return session.get("username") != null;
+    }
+
+    public static String getCurrentUsername() {
+        User currentUser = User.find("byEmail", session.get("username")).first();
+        return isConnected() ? currentUser.username : "null";
+    }
     public static List<Subberry> getHighestSubberries() {
         return Subberry.find("order by nbUsers desc").from(0).fetch(3);
     }
 
     public static void index() {
+        String username = getCurrentUsername();
         List<Subberry> highestSubberries = getHighestSubberries();
         List<Post> mostRecentPosts = Post.find("order by postedAt desc").from(0).fetch(3);
-        render(highestSubberries, mostRecentPosts);
+        render(highestSubberries, username, mostRecentPosts);
     }
 
     public static void showSub(Long id) {
+        String username = getCurrentUsername();
         List<Subberry> highestSubberries = getHighestSubberries();
         Subberry sub = Subberry.findById(id);
-        render(highestSubberries, sub);
+        render(highestSubberries, username, sub);
     }
 
     public static void showPost(Long id) {
+        String username = getCurrentUsername();
         List<Subberry> highestSubberries = getHighestSubberries();
         Post post = Post.findById(id);
-        render(highestSubberries, post);
+        render(highestSubberries, username, post);
     }
 
     public static void addPost(Long id, String title, String content) {
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,10 +6,13 @@ let secondSubTitle = index_js.attr('data-secondHighestSubTitle')
 let secondSubId = index_js.attr('data-secondHighestSubId')
 let thirdSubTitle = index_js.attr('data-thirdHighestSubTitle')
 let thirdSubId = index_js.attr('data-thirdHighestSubId')
+let username = index_js.attr('data-username')
 
 
 $(function() {
     let navbarNav = document.getElementById("navbar-nav")
+    let loggedOutMenu = document.getElementById("logged-out-menu")
+    let loggedInMenu = document.getElementById("logged-in-menu")
     let firstLink = navbarNav.children.item(0)
     $(firstLink).children('a')[0].innerHTML = firstSubTitle
     $(firstLink).children('a')[0].setAttribute('href', "/sub/" + firstSubId)
@@ -19,4 +22,10 @@ $(function() {
     let thirdLink = navbarNav.children.item(2)
     $(thirdLink).children('a')[0].innerHTML = thirdSubTitle
     $(thirdLink).children('a')[0].setAttribute('href', '/sub/' + thirdSubId)
+    if (username === "null") {
+        loggedInMenu.style.display = "none"
+    } else {
+        loggedInMenu.children.item(0).children.item(0).innerHTML = username
+        loggedOutMenu.style.display = "none"
+    }
 });
